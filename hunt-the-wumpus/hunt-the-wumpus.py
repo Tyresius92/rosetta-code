13f06b2af895a4f500ee07c0ba40452788c9ac7d
Tyresius92 <tyrelclayton92@gmail.com>
2018-05-25 16:53:54 -0400

    finished Hunt The Wumpus! still needs light refactoring

--- hunt-the-wumpus/hunt-the-wumpus.py
+++ hunt-the-wumpus/hunt-the-wumpus.py
@@ -60,7 +60,7 @@ def print_turn_msgs():
     if msg_printed == False:
         print("It is eerily silent. You sense nothing nearby.")
 
-    print("You have " + str(num_arrows) + " arrows remaining.")
+    print("You have " + str(num_arrows) + " arrows remaining.\n")
 
 def get_player_action():
     print("Would you like to shoot, or enter another room?")
@@ -93,7 +93,77 @@ def get_player_room_choice(action):
             print("That is not a valid room number.")
             print("Please enter 1, 2, or 3.")
 
-    return int(room_choice)
+    return int(room_choice) - 1
+
+def enter_room(rooms, room_num):
+    player_rm = rooms.index(PLAYER)
+    goto_rm = room_connections[player_rm][room_num]
+
+    if rooms[goto_rm] == WUMPUS:
+        print("You have stumbled into the lair of the Wumpus!")
+        print("He leaps on you and gobbles you up!")
+        print("You have lost.\n")
+        game_over = True
+    elif rooms[goto_rm] == PIT:
+        print("You enter the room, and fall into a bottomless pit!")
+        print("You fall forever.")
+        print("You have lost.\n")
+        game_over = True
+    elif rooms[goto_rm] == BAT:
+        print("Upon entering the room, a huge bat grabs you!")
+        print("It carries you to a different room.\n")
+        rooms[player_rm] = None
+        player_rm = get_empty_room(rooms)
+        rooms[player_rm] = PLAYER
+        game_over = False
+    elif rooms[goto_rm] == None:
+        print("You enter another room.\n")
+        rooms[player_rm] = None
+        player_rm = room_connections[player_rm][room_num]
+        rooms[player_rm] = PLAYER
+        game_over = False
+
+    return game_over
+
+def shoot_room(rooms, room_num, num_arrows):
+    player_rm = rooms.index(PLAYER)
+    shoot_rm = room_connections[player_rm][room_num]
+
+    if rooms[shoot_rm] == WUMPUS:
+        print("You have shot and killed the Wumpus!")
+        print("You win!\n")
+        game_over = True
+    elif rooms[shoot_rm] == PIT:
+        print("You shoot into the room and lose an arrow.\n")
+        game_over = False
+    elif rooms[shoot_rm] == BAT:
+        print("You have killed a bat!\n")
+        rooms[shoot_rm] = None
+        game_over = False
+    elif rooms[shoot_rm] == None:
+        print("You shoot into the room and lose an arrow.\n")
+        game_over = False
+
+    if not game_over:
+        num_arrows -= 1
+
+        if (random.randint(1, 4) % 4) == 0:
+            wump_rm = rooms.index(WUMPUS)
+            rooms[wump_rm] = None
+            wump_rm = room_connections[wump_rm][random.randint(0, 2)]
+            if (wump_rm == player_rm):
+                print("The Wumpus enters the room with you and gobbles you up!")
+                print("You have lost.\n")
+                game_over == True
+            else:
+                rooms[wump_rm] = WUMPUS
+
+        elif num_arrows == 0:
+            print("You have run out of arrows!")
+            print("You have lost.")
+            game_over = True
+
+    return game_over, num_arrows
 
 room_connections = [ [ 7, 13, 9 ],
                      [ 12, 18, 0 ],
@@ -116,31 +186,36 @@ room_connections = [ [ 7, 13, 9 ],
                      [ 1, 3, 9 ],
                      [ 2, 8, 14 ] ]
 
-rooms = populate_rooms()
 
-num_arrows = 5
 game_over = False
 play_again = True
 
-print("Welcome to Hunt the Wumpus!")
-
-while not game_over:
-    print_turn_msgs()
+while play_again:
+    print("Welcome to Hunt the Wumpus!\n")
+    rooms = populate_rooms()
+    num_arrows = 5
     
-    action = get_player_action()
-    
-
-    if action == QUIT:
-        game_over = True
-        print("Thank you for playing Hunt the Wumpus!")
-        print("Have a great day!")
-
-    room_choice = get_player_room_choice(action)
-    if action == SHOOT:
+    while not game_over:
+        print_turn_msgs()
         
+        action = get_player_action()
 
-        
-    if action == ENTER:
-        enter_room(room_choice)
+        if action == QUIT:
+            game_over = True
+            play_again = False
+            print("Thank you for playing Hunt the Wumpus!")
+            print("Have a great day!")
+            break
 
+        room_choice = get_player_room_choice(action)
+        if action == SHOOT:
+            game_over, num_arrows = shoot_room(rooms, room_choice, num_arrows)
+            
+        if action == ENTER:
+            game_over = enter_room(rooms, room_choice)
 
+    if action != QUIT:
+        print("Would you like to play again? [y/n]")
+        play_again = input().lower().startswith('y')
+        if play_again:
+            game_over = False
